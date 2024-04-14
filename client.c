b740575ba4584025f42d66264f775996b52160d1
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2024-04-14 19:20:11 +0100

    alteração na pipeline

--- client.c
+++ client.c
@@ -5,25 +5,31 @@
 int main(int argc, char** argv){
 	/*if(argc <= 1){
 		printf("Opções:\n");
-		printf("./client execute [time] -u \"[prog] [args]\"\n");
-		printf("./client execute [time] -p \"prog-1 [args] | ... | prog-n [args]\"\n");
+		printf("./client [time] -u \"[prog] [args]\"\n");
+		printf("./client [time] -p \"prog-1 [args] | ... | prog-n [args]\"\n");
 		printf("./client status\n");
 		
 	}*/
 	
-	int fd1 = open("pipe_in", O_WRONLY);
-	int fd2 = open("pipe_out", O_RDONLY);
-	
+	char pipe_name[7];
 	char buffer[buffer_size];
-	//char aux[buffer_size];
-	ssize_t read_bytes, bytes;	
-	read_bytes=read(0, &buffer, buffer_size);
-	write(fd1, &buffer, read_bytes);
-	while((bytes=read(fd2, &buffer, buffer_size))>0){
-		write(1, &buffer, bytes);
-	}
-	close(fd1);
-	//close(fd2);
+	ssize_t read_bytes;
+	
+	srand(time(NULL));
+	int x = rand() % 100;
+	sprintf(pipe_name, "pipe%d", x);
+	
+	mkfifo(pipe_name, 0666);
+	
+	int server_pipe = open("server_pipe", O_WRONLY);
+	write(server_pipe, &pipe_name, 7);
+	
+	int fd = open(pipe_name, O_RDONLY);
+	read_bytes = read(fd, &buffer, buffer_size);
+	write(1, &buffer, read_bytes);
+	close(fd);
+	close(server_pipe);
+	unlink(pipe_name);
 	
 	return 0;
 }
