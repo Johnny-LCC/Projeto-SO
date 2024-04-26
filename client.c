b6bf69d92cbe780bf8004eb5af60fa52ca0d690c
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2024-04-26 23:47:17 +0100

    Update client.c

--- client.c
+++ client.c
@@ -4,7 +4,7 @@
 
 void func(int n, char **m, char *s){
 	int p = 0;
-	for(int i = 1; i<n; i++){
+	for(int i = 4; i<n; i++){
 		for(long unsigned int j = 0; j<strlen(m[i]); j++){
 			s[p]= m[i][j];
 			p++;
@@ -21,13 +21,24 @@ int main(int argc, char** argv){
 		printf("./client execute [time] -u \"[prog] [args]\"\n");
 		printf("./client execute [time] -p \"prog-1 [args] | ... | prog-n [args]\"\n");
 		printf("./client status\n");
-		
+		return -1;
 	}
 	
 	Task task;
 	task.pid_client = getpid();
-	func(argc, argv, task.args);
-	task.status = Sent;
+	if(strcmp(argv[1], "execute") == 0){
+		task.time = atoi(argv[2]);
+		if(strcmp(argv[3], "-p")==0) task.pipe = 1;
+		else task.pipe = 0;
+		func(argc, argv, task.args);
+		//task.status = Scheduled;
+	}
+	else if(strcmp(argv[1], "status") == 0){
+		task.time = task.pipe = 0;
+		strcpy(task.args, argv[1]);
+		//task.status = Executing;
+	}
+	else return -1;
 	
 	char pipe_name[6];
 	char buffer[buffer_size];
@@ -42,6 +53,7 @@ int main(int argc, char** argv){
 	int fd = open(pipe_name, O_RDONLY);
 	read_bytes = read(fd, &buffer, buffer_size);
 	write(1, &buffer, read_bytes);
+	
 	close(fd);
 	close(server_pipe);
 	unlink(pipe_name);
